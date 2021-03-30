5abfb7118446f0b8c4999aa89bf0bf07e2e01ee6
Tierney Cyren <hello@bnb.im>
2021-03-29 20:09:12 -0400

    chore: refine GitHub events

--- index.js
+++ index.js
@@ -1,16 +1,18 @@
 const dedupeLocalEventsFilesInDirectory = require('./lib/dedupeLocalEventsFilesInDirectory')
-const getAndFilterPublicGitHubEvents = require('./lib/getAndFilterPublicGitHubEvents')
+const filterPublicGitHubEvents = require('./lib/filterPublicGitHubEvents')
 const getEventsFrom = require('./lib/getEventsFrom')
 const getMarkdownFromEvents = require('./lib/getMarkdownFromEvents')
+const getPublicGitHubEvents = require('./lib/getPublicGitHubEvents')
 const writeEventsFile = require('./lib/writeEventsFile')
 const writeMarkdownFile = require('./lib/writeMarkdownFile')
 const writeMegafile = require('./lib/writeMegafile')
 
 module.exports = {
   dedupeLocalEventsFilesInDirectory,
-  getAndFilterPublicGitHubEvents,
+  filterPublicGitHubEvents,
   getEventsFrom,
   getMarkdownFromEvents,
+  getPublicGitHubEvents,
   writeEventsFile,
   writeMarkdownFile,
   writeMegafile
