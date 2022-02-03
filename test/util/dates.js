67b178fa569d2e513cc567043129a60dcb7d6453
Tierney Cyren <hello@bnb.im>
2022-02-02 21:20:13 -0500

    chore: some test improvements
    Signed-off-by: Tierney Cyren <hello@bnb.im>

--- test/util/dates.js
+++ test/util/dates.js
@@ -1,14 +1,15 @@
 const { DateTime } = require('luxon')
 
 const now = DateTime.now()
+const currentYear = now.year
 const lastMonth = now.minus({ months: 1 })
 const lastDayOfPreviousMonth = lastMonth.set({ day: lastMonth.daysInMonth })
 
 const dates = {
   now: now.toISODate(), // make `now` usable with ISO date format
   lastDayOfPreviousMonth: lastDayOfPreviousMonth.toISODate(), // gets the last day of the previous month
-  checkFrom: `2021-${lastDayOfPreviousMonth.month.toString().padStart(2, 0)}-01`, // yyyy-mm-dd
-  checkUntil: `2021-${lastDayOfPreviousMonth.month.toString().padStart(2, 0)}-${lastDayOfPreviousMonth.daysInMonth.toString().padStart(2, 0)}` // yyyy-mm-dd-yyyy-mm-dd
+  checkFrom: `${currentYear}-${lastDayOfPreviousMonth.month.toString().padStart(2, 0)}-01`, // yyyy-mm-dd
+  checkUntil: `${currentYear}-${lastDayOfPreviousMonth.month.toString().padStart(2, 0)}-${lastDayOfPreviousMonth.daysInMonth.toString().padStart(2, 0)}` // yyyy-mm-dd-yyyy-mm-dd
 }
 
 module.exports = dates
--- test/util/headings.js
+++ test/util/headings.js
@@ -1,7 +1,7 @@
 function createHeadings (dates) {
   const headings = {
-    monthly: `Open Source Engineering Report: Monthly Run at ${dates.now}`,
-    weekly: `Open Source Engineering Report: Weekly Run at ${dates.now}`
+    monthly: `Personal GitHub Activity Report: Monthly Run at ${dates.now}`,
+    weekly: `Personal GitHub Activity Report: Weekly Run at ${dates.now}`
   }
 
   return headings
